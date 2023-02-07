e0a13306535b9219b77653954fd37f1c9b1368e7
Adam Pierson <adam.pierson@jupiterone.com>
2023-02-07 09:01:08 -0500

    Handling case where some VMs aren't able to return guest identity results

--- src/steps/virtual-machine/index.ts
+++ src/steps/virtual-machine/index.ts
@@ -1,10 +1,12 @@
 import {
   IntegrationStep,
   IntegrationStepExecutionContext,
+  IntegrationWarnEventName,
 } from '@jupiterone/integration-sdk-core';
 
 import { getOrCreateAPIClient } from '../../client';
 import { IntegrationConfig } from '../../config';
+import { VsphereGuestInfo, VsphereGuestInfoDeprecated } from '../../types';
 import { Steps, Entities } from '../constants';
 import { createVmEntity } from './converter';
 
@@ -14,11 +16,32 @@ export async function fetchVms({
   logger,
 }: IntegrationStepExecutionContext<IntegrationConfig>) {
   const apiClient = getOrCreateAPIClient(instance.config, logger);
+  let guestQueryFailCount: number = 0;
+  let guestQuerySuccessCount: number = 0;
+  let vmGuest: VsphereGuestInfo | VsphereGuestInfoDeprecated | null;
 
   await apiClient.iterateVms(async (vm) => {
-    const vmGuest = await apiClient.getVmGuest(vm.vm as string);
+    // Wrap the VM Guest call in a try block, since it may give a 503 error
+    // if we don't have VMware tools enabled on some VMs.
+    try {
+      vmGuest = await apiClient.getVmGuest(vm.vm as string);
+      guestQuerySuccessCount++;
+    } catch (err) {
+      logger.info(
+        `Unable to query vcenter/vm/${vm.vm}/guest/identity endpoint.  This may be due to permissions or having VMware tools turned off on that VM.`,
+      );
+      vmGuest = null;
+      guestQueryFailCount++;
+    }
     await jobState.addEntity(createVmEntity(vm, vmGuest));
   });
+
+  if (guestQueryFailCount > 0) {
+    logger.publishWarnEvent({
+      name: IntegrationWarnEventName.MissingPermission,
+      description: `Could not query all guest information for VMs.  Success = ${guestQuerySuccessCount}  Failed = ${guestQueryFailCount}`,
+    });
+  }
 }
 
 export const vmSteps: IntegrationStep<IntegrationConfig>[] = [
