238cd5e1feaa4e59d50eb4928132408e7b65537f
dsalazar-talend <71341528+dsalazar-talend@users.noreply.github.com>
2023-04-03 14:37:12 -0700

    Updated index.ts
    Added logic to query getVm API endpoint to pull in vmDetails.identity.bios_uuid data for each VM.
    Updated addEntity() to include the new data point, to be passed off to the converter.

--- src/steps/virtual-machine/index.ts
+++ src/steps/virtual-machine/index.ts
@@ -19,7 +19,11 @@ export async function fetchVms({
   let guestQueryFailCount: number = 0;
   let guestQuerySuccessCount: number = 0;
   let vmGuest: VsphereGuestInfo | VsphereGuestInfoDeprecated | null;
-
+  let vmDetails: VsphereVmDetails | VsphereVmDetailsDeprecated | null;
+  let bios_uuid: string;
+  let detailsQueryFailCount: number = 0;
+  let detailsQuerySuccessCount: number = 0;
+  
   await jobState.iterateEntities(
     { _type: Entities.HOST._type },
     async (hostEntity) => {
@@ -37,7 +41,18 @@ export async function fetchVms({
           vmGuest = null;
           guestQueryFailCount++;
         }
-        await jobState.addEntity(createVmEntity(vm, vmGuest));
+        try {
+          vmDetails = await apiClient.getVm(vm.vm as string);
+          bios_uuid = vmDetails.identity?.bios_uuid;
+          detailsQuerySuccessCount++;
+        } catch (err) {
+          logger.info(
+            `Unable to query vcenter/vm/${vm.vm} endpoint.`,
+          );
+          vmDetails = null;
+          detailsQueryFailCount++;
+        }
+        await jobState.addEntity(createVmEntity(vm, vmGuest, bios_uuid));
       }, hostEntity.hostname as string);
     },
   );
