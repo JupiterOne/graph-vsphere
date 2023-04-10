3a4393991fd9117cf0af21ed8537891a0636a57b
dsalazar-talend <71341528+dsalazar-talend@users.noreply.github.com>
2023-04-10 14:19:16 -0700

    Update converter.ts
    Fixed formatting issue

--- src/steps/virtual-machine/converter.ts
+++ src/steps/virtual-machine/converter.ts
@@ -14,7 +14,7 @@ export function createVmEntity(
   vm: VsphereVm,
   vmGuest: VsphereGuestInfo | VsphereGuestInfoDeprecated | null,
   bios_uuid: string,
-  mac_address: string[],   
+  mac_address: string[],
 ): Entity {
   return createIntegrationEntity({
     entityData: {
